eb7895f11e00066bafaf8ce4479bb79ad16a088f
Lung-Chen Huang <perfecthuang413@gmail.com>
2019-02-15 23:32:35 +0800

    Change Heap structure

--- include/utils/Heap.hpp
+++ include/utils/Heap.hpp
@@ -2,79 +2,79 @@
 
 #include <vector>
 #include <mutex>
+#include <Asset.hpp>
 
 namespace utils {
     /*
         MinHeap & MaxHeap currently only support numeric types as template arguments;
     */
+    using namespace trading;
 
-    template <bool isMin, class T, class X, class A>
+    template <class A>
+    struct is_asset_ptr {
+        static const bool val = false;
+    };
+
+    template <>
+    struct is_asset_ptr <AssetPtr> {
+        static const bool val = true;
+    };
+
+    template <bool isMin, class A>
     class Heap {
         public:
+            Heap()
+            {
+                static_assert(is_asset_ptr<A>::val, "'Asset' type needed");
+            }
             A getTop()
             {
                 _heapLock.lock();
                 A data;
-                if (_valHeap.size() == 0) {
+                if (_ptrHeap.size() == 0) {
                     return data;
                 }
-                data.val = _valHeap.at(0);
-                data.id = _nameHeap.at(0);
+                data = _ptrHeap.at(0);
                 _heapLock.unlock();
                 return data;
             }
-            void insert(const T &e, const X &n)
+            void insert(A e)
             {
+                static_assert(is_asset_ptr<A>::val, "'Asset' type needed");
                 _heapLock.lock();
-                _valHeap.push_back(e);
-                _nameHeap.push_back(n);
-                bubbleUp(_valHeap.size() - 1);
+                _ptrHeap.push_back(e);
+                bubbleUp(_ptrHeap.size() - 1);
                 _heapLock.unlock();
             }
             void bubbleUp(int idx)
             {
                 if (idx == 0) return;
                 int nextIdx = (idx - 1) / 2;
-                T &e1 = _valHeap.at(idx);
-                T &e2 = _valHeap.at(nextIdx);
-                X &n1 = _nameHeap.at(idx);
-                X &n2 = _nameHeap.at(nextIdx);
+                A &e1 = _ptrHeap.at(idx);
+                A &e2 = _ptrHeap.at(nextIdx);
+                bool toSwap = false;
                 if (isMin) {
-                    if (e1 < e2) {
-                        T _e1 = e1;
-                        T _e2 = e2;
-                        X _n1 = n1;
-                        X _n2 = n2;
-                        e1 = _e2;
-                        e2 = _e1;
-                        n1 = _n2;
-                        n2 = _n1;
-                        bubbleUp(nextIdx);
-                    }
+                    if (e1->val < e2->val) toSwap = true;
                 } else {
-                    if (e1 > e2) {
-                        T _e1 = e1;
-                        T _e2 = e2;
-                        X _n1 = n1;
-                        X _n2 = n2;
-                        e1 = _e2;
-                        e2 = _e1;
-                        n1 = _n2;
-                        n2 = _n1;
-                        bubbleUp(nextIdx);
-                    }
+                    if (e1->val > e2->val) toSwap = true;
+                }
+                if (toSwap) {
+                    A _e1 = e1;
+                    A _e2 = e2;
+                    e1 = _e2;
+                    e2 = _e1;
+                    bubbleUp(nextIdx);
                 }
             }
         private:
             std::mutex _heapLock;
-            std::vector<T> _valHeap;
-            std::vector<X> _nameHeap;
+            std::vector<A> _ptrHeap;
     };
 
-    template<class T, class X, class A>
-    using MinHeap = Heap<true, T, X, A>;
+    template <class A>
+    using MinHeap = Heap<true, A>;
 
-    template<class T, class X, class A>
-    using MaxHeap = Heap<false, T, X, A>;
+    template <class A>
+    using MaxHeap = Heap<false, A>;
 
 }
