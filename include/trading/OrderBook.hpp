34ebe89c91e720b223634d7b24249a6ff87a4f86
Lung-Chen Huang <perfecthuang413@gmail.com>
2019-02-16 00:02:08 +0800

    Explain use of Heaps

--- include/trading/OrderBook.hpp
+++ include/trading/OrderBook.hpp
@@ -13,6 +13,12 @@ namespace trading {
             AssetPtr getH(OrderType);
             void add(OrderType, double, double, const std::string &);
         private:
+            /*
+            *   The following Heaps are used to maintain the min or max of any OrderType for a specific asset pair
+            *   since the JSON data received from online exchanges may not be in the expected order.
+            *   Thus, it always ensures to gain as many profits as possible.
+            *   However, _buyMinHeap and _sellMaxHeap would not be useful in this case.
+            */
             utils::MinHeap<AssetPtr> _buyMinHeap;
             utils::MaxHeap<AssetPtr> _buyMaxHeap;
             utils::MinHeap<AssetPtr> _sellMinHeap;
